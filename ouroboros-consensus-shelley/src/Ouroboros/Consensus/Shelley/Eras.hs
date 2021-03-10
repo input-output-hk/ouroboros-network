619dd89b268917fc6fcc7d6eb0c4dd35a8b62f33
EncodePanda <paul.szulc@gmail.com>
2021-03-10 15:21:54 +0100

    [WIP] Integrate alonzo/impl

--- ouroboros-consensus-shelley/src/Ouroboros/Consensus/Shelley/Eras.hs
+++ ouroboros-consensus-shelley/src/Ouroboros/Consensus/Shelley/Eras.hs
@@ -32,6 +32,8 @@ import           Cardano.Ledger.Allegra (AllegraEra)
 import qualified Cardano.Ledger.Core as LC
 import           Cardano.Ledger.Era (Crypto)
 import           Cardano.Ledger.Mary (MaryEra)
+import           Cardano.Ledger.Alonzo (AlonzoEra)
+import qualified Cardano.Ledger.Alonzo.PParams
 import           Cardano.Ledger.Shelley (ShelleyEra)
 import           Control.State.Transition (State)
 
@@ -39,14 +41,6 @@ import qualified Cardano.Ledger.Shelley.Constraints as SL
 import           Ouroboros.Consensus.Shelley.Protocol.Crypto (StandardCrypto)
 import qualified Shelley.Spec.Ledger.API as SL
 import qualified Shelley.Spec.Ledger.BaseTypes as SL
-
--- | Temporary workaround
--- Currently AlonzoEra from 'cardano-ledger-alonzo' is not ready to be used
--- We use this type alias to temporarily work around this issue.
--- Once 'AlonzoEra' from 'cardano-ledger-alonzo' is ready we will remove
--- this workaround and replace it with the proper one from
--- the 'import Cardano.Ledger.Alonzo (AlonzoEra)''
-type AlonzoEra c = MaryEra c
 
 {-------------------------------------------------------------------------------
   Eras instantiated with standard crypto
@@ -124,3 +118,6 @@ instance SL.PraosCrypto c => ShelleyBasedEra (AllegraEra c) where
 
 instance SL.PraosCrypto c => ShelleyBasedEra (MaryEra c) where
   shelleyBasedEraName _ = "Mary"
+
+instance SL.PraosCrypto c => ShelleyBasedEra (AlonzoEra c) where
+  shelleyBasedEraName _ = "Alonzo"
